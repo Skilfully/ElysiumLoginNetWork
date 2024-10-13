d2d7b3c2c2e851ebb9d56e9e4ecee035b8ea1c87
bulrfire <2592718594@qq.com>
2024-10-13 18:04:48 +0800

    建立项目

---
+++ src/main/java/cn/skilfully/elysiumloginnetwork/ElysiumLoginNetWork.java
@@ -0,0 +1,17 @@
+package cn.skilfully.elysiumloginnetwork;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ElysiumLoginNetWork extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
